fda43c61c905bf8cd9707ec12e865273c4b435e2
Nishid Jojare <developer.nishid@gmail.com>
2024-02-05 15:20:09 +0530

    done with  Initial setup

---
+++ student.py
@@ -0,0 +1,15 @@
+import mysql.connector
+try:
+    mydb=mysql.connector.connect(host="localhost",user="root",password="",database="pythonprojectsdb")
+    
+    mycursor=mydb.cursor()
+    mycursor.execute('SELECT * FROM tblcourse')
+    data=mycursor.fetchall()
+    for row in data:
+        print(row)
+        
+except:
+    pass 
+
+
+
